ab7bbe8a2c66e50d7248b0eab7bf000112beea07
vagrant <vagrant@precise32.(none)>
2014-03-22 11:27:34 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fixaudit::Application.config.secret_token = 'fdde015cb5c32213b25f134fe7d0a60ff405555f5a26af697ddd53b8485ce573f87dd55b5c27f517ea54bbf8b70a2862a13acf200ad1c710e8f59f2e8406859d'
